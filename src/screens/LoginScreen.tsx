b7caa3c7fd47eee3f6e0bff4b97199e21e08cf44
zveresch2019-ship-it <zveresch2019@gmail.com>
2025-11-12 13:09:08 +0100

    Expand login help width to 510

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -531,7 +531,7 @@ const styles = StyleSheet.create({
   },
   content: {
     width: '100%',
-    maxWidth: 520,
+    maxWidth: 540,
     alignItems: 'stretch',
     gap: 8,
   },
@@ -544,7 +544,7 @@ const styles = StyleSheet.create({
   },
   helpContainer: {
     width: '100%',
-    maxWidth: 500,
+    maxWidth: 510,
     alignSelf: 'center',
     marginTop: -8,
     height: 660,
